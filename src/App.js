b65ddabb2e05dd813936837c64a5515a1e45ba13
tatyana12 <tatyana_pol@yahoo.com>
2018-11-15 19:29:47 -0800

    make it better

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './events.json';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+           <ul>
+            { data.map(function(people){
+                return <li>{people.id} - {people.name}</li>
+             })}
+             </ul>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
